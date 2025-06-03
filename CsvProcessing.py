c49aedc41ecbb14d08b3faea0d761d8038ad5a56
LAPTOP-DQJA02LK\willi <williamgutschmidt27@gmail.com>
2025-06-03 09:44:30 +0200

    changed processing to right sensor format

--- CsvProcessing.py
+++ CsvProcessing.py
@@ -1,38 +1,53 @@
 import csv
  
 def process_csv_data(folder):
-    max_temperature = float('-inf')
-    min_temperature = float('inf')
-    list_of_temperatures = []
+    max_pollution_1 = float('-inf')
+    max_pollution_2 = float('-inf')
+    min_pullution_p1 = float('inf')
+    min_pullution_p2 = float('inf')
+    all_pollution_p1 = 0
+    all_pollution_p2 = 0
+    list_of_polution_P1 = []
+    list_of_polution_P2 = []
     list_of_time = []
     list_of_dates = []
-    all_temp = 0
  
     with open (folder, "r") as file:
         data = csv.DictReader(file, delimiter=";")
         i = 0
         for row in data:
             i += 1
-            current_temperature = float(row["temperature"])
-            list_of_temperatures.append(current_temperature)
+            current_pollution_P1 = float(row["P1"])
+            current_pollution_P2 = float(row["P2"])
+            list_of_polution_P1.append(current_pollution_P1)
+            list_of_polution_P2.append(current_pollution_P2)
             time = row["timestamp"].split("T")
             list_of_time.append(time[1])
             list_of_dates.append(time[0])
            
-            if current_temperature > max_temperature:
-                max_temperature = current_temperature
-            if current_temperature < min_temperature:
-                min_temperature = current_temperature
-               
-            all_temp += float(current_temperature)
+            if current_pollution_P1 > max_pollution_1:
+                max_pollution_1 = current_pollution_P1
+            if current_pollution_P1 < min_pullution_p1:
+                min_pullution_p1 = current_pollution_P1
 
-        average_temperature = all_temp / i
-        return [
-            max_temperature,
-            min_temperature,
-            average_temperature,
-            list_of_temperatures,
-            list_of_time,
-            list_of_dates
-        ]
-print(process_csv_data("2024-01-20_bme280_sensor_141.csv"))
+            if current_pollution_P2 > max_pollution_2:
+                max_pollution_2 = current_pollution_P2
+            if current_pollution_P2 < min_pullution_p2:
+                min_pullution_p2 = current_pollution_P2
+               
+            all_pollution_p1 += float(current_pollution_P1)
+            all_pollution_p2 += float(current_pollution_P2)
+
+        average_P1 = all_pollution_p1 / i
+        average_P2 = all_pollution_p2 / i
+        return {
+            "max_pollution_1 ": max_pollution_1,
+            "max_pollution_2 ": max_pollution_2,
+            "min_pullution_p1 ": min_pullution_p1,
+            "min_pullution_p2 ": min_pullution_p2,
+            "average_P1": average_P1,
+            "average_P2": average_P2,
+            "all_pollutions_1 ": list_of_polution_P1,
+            "all_pollutions_2 ": list_of_polution_P2,
+        }
+print(process_csv_data("2024-01-20_sds011_sensor_31128.csv"))
