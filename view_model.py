e98400b90a37169ea4af8d642783e75027bd23af
Jannik Sauer <Jannik.sauer@common-solutions.de>
2025-06-14 14:14:50 +0200

    dnb

--- view_model.py
+++ view_model.py
@@ -1,4 +1,4 @@
-from csv_download import download_csv_files
+from CSV_Download import download_csv_files
 
 def download_data(start_date, end_date, sensor_type, sensor_id, folder):
     download_csv_files(start_date, end_date, sensor_type, sensor_id, folder)
